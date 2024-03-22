009947da1c5efad72f726be397fdf5840745acce
Wes MacDonald <wrmacdonald2016@gmail.com>
2024-03-22 09:54:07 -0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Wes MacDonald on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
